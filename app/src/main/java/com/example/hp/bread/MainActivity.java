4a786943bc8cdcfacf4efee727e1ccce985ce053
Matthew <mattjinks@gmail.com>
2018-11-26 14:53:07 -0500

    Landing Page Needs to be Formatted for Presentation

--- app/src/main/java/com/example/hp/bread/MainActivity.java
+++ app/src/main/java/com/example/hp/bread/MainActivity.java
@@ -3,71 +3,48 @@ package com.example.hp.bread;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.app.Notification;
+import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity
-        implements
-        Search.OnFragmentInteractionListener,
-        Favorites.OnFragmentInteractionListener,
-        Account.OnFragmentInteractionListener {
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-        //leave empty
-    }
-
-    private BottomNavigationView mainNav;
-    private FrameLayout mainFrame;
-
-    private Search searchFragment;
-    private Account accountFragment;
-    private Favorites favoritesFragment;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mainNav = (BottomNavigationView) findViewById(R.id.main_nav);
-        mainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        BottomNavigationView bottomNavigationView =
+                (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
 
-        searchFragment = new Search();
-        accountFragment = new Account();
-        favoritesFragment = new Favorites();
-
-        mainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
-
-                    case R.id.nav_search :
-                        setFragment(searchFragment);
-                        return true;
-
-                    case R.id.nav_account :
-                        setFragment(accountFragment);
-                        return true;
-
-                    case R.id.nav_favorites :
-                        setFragment(favoritesFragment);
-                        return true;
-
-                    default:
-                        return false;
+                    case R.id.ic_search:
+                        Intent searchIntent = new Intent(MainActivity.this,
+                                SearchActivity.class);
+                        break;
+                    case R.id.ic_user_account:
+                        Intent accountIntent = new Intent(MainActivity.this,
+                                UserAccountActivity.class);
+                        startActivity(accountIntent);
+                        break;
+                    case R.id.ic_favorites:
+                        Intent favIntent = new Intent(MainActivity.this,
+                                FavoritesActivity.class);
+                        startActivity(favIntent);
+                        break;
                 }
+
+                return false;
             }
         });
-    }
-
-    private void setFragment(android.support.v4.app.Fragment fragment) {
-        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.main_frame, fragment);
-        fragmentTransaction.commit();
     }
 }
